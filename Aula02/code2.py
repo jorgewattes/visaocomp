51614c6588ce3fa7bde51bcc89cd7e67cfbe7970
jorgewattes <jorgelwoj@hotmail.com>
2022-04-25 16:59:45 -0300

    Aula02 done

--- Aula02/code2.py
+++ Aula02/code2.py
@@ -7,12 +7,11 @@ cap = cv.VideoCapture(-1)
 # fps = int(cap.get(cv.CAP_PROP_FPS))
 # frames=int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 
-ret,frame = cap.read()
-cv.imshow('Frame',frame)
+while(True):
+    ret,frame = cap.read()
 
-# while(True):
-#     ret,frame = cap.read()
-
-#     cv.imshow('Frame',frame)
+    cv.imshow('Frame',frame)
+    if(cv.waitKey(1) & 0xFF ==ord('q')):
+        break
 cap.release()
 cv.destroyAllWindows()
